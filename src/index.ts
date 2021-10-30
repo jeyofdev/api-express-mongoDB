6aca6d7e243fc2839d4b81e5cafc2a9ad9630828
jeyofdev <jgregoire.pro@gmail.com>
2021-10-30 10:52:13 +0200

    feat : init api with express

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,26 @@
-console.log('ok');
+import express from 'express';
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
 
-const testForLinter = 'test';
+dotenv.config();
+
+// Config
+const PORT: Number = Number(process.env.PORT) || 4000;
+const app = express();
+
+// Connect MongoDb
+const connectOptions = { autoIndex: true };
+mongoose
+  .connect(<string>process.env.MONGO_URL, connectOptions)
+  .then(() => console.log('Connected to database MongoDB'))
+  .catch((err) => console.log(err));
+
+// Routes
+app.get('/', (req, res) => {
+  res.status(200).json({ message: 'Welcome to the API' });
+});
+
+// Listen
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
